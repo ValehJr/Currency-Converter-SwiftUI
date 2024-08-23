bef125b5efb9dce22c77d50045311ac043070d19
Valeh <i.valehjr@gmail.com>
2024-08-23 16:04:20 +0400

    Initial Commit

---
+++ CurrencyConverter/CurrencyConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  CurrencyConverterApp.swift
+//  CurrencyConverter
+//
+//  Created by Valeh Ismayilov on 23.08.24.
+//
+
+import SwiftUI
+
+@main
+struct CurrencyConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
